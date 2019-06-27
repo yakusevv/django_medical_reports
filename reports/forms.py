b611ae384eff01f4df8daf14d7baf4214f2488e6
yakusevv <yakusevv@gmail.com>
2019-06-27 21:00:30 +0200

    Report create formset validation

--- reports/forms.py
+++ reports/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.forms.models import inlineformset_factory
+from django.forms.models import BaseInlineFormSet
 
 from .models import Report, ServiceItem
 
@@ -32,6 +33,13 @@ class ReportCreateForm(forms.ModelForm):
         self.user = kwargs.pop('doctor')
         super(ReportCreateForm, self).__init__(*args, **kwargs)
 
+    def clean(self):
+        cleaned_data=super(ReportCreateForm, self).clean()
+#        for form in self.formset:
+#            if not form.is_valid():
+#                raise forms.ValidationError('some error')
+        return cleaned_data
+
 class ServiceItemForm(forms.ModelForm):
 
     class Meta:
@@ -41,7 +49,32 @@ class ServiceItemForm(forms.ModelForm):
             'quantity'
             ]
 
+def service_formset_validating():
+    class UniqueFieldFormSet(BaseInlineFormSet):
+
+        def clean(self):
+            if any(self.errors):
+                return
+            services = set()
+            number_of_forms = 0
+            for form in self.forms:
+                if form.cleaned_data:
+                    if not form.cleaned_data['DELETE']:
+                        service = form.cleaned_data['service']
+                        quantity = form.cleaned_data['quantity']
+                        number_of_forms += 1
+                        if service in services:
+                            form.add_error('service','Duplicate values for "Service" are not allowed.')
+                        else:
+                            services.add(service)
+                        if quantity < 1:
+                            form.add_error('quantity', "Quantity must be equal to or greater than 1")
+            if number_of_forms == 0:
+                raise forms.ValidationError('At least one service must be chosen')
+
+    return UniqueFieldFormSet
 
 ServiceItemsFormSet = inlineformset_factory(
-                    Report, ServiceItem,
-                    form=ServiceItemForm, extra=1)
+                    Report, ServiceItem, formset=service_formset_validating(),
+                    form=ServiceItemForm, extra=1
+                    )
--- reports/views.py
+++ reports/views.py
@@ -14,46 +14,28 @@ class ReportDetailView(DetailView):
     model = Report
     template_name = 'reports/report_detail.html'
 
-'''
-class ReportCreateView(CreateView):
-    template_name = 'reports/report_create.html'
-
-    def get_context_data(self, **kwargs):
-        data = super(ReportCreateView, self).get_context_data(**kwargs)
-        if self.request.POST:
-            data['service_items'] = ServiceItemsFormSet(self.request.POST)
-        else:
-            data['service_items'] = ServiceItemsFormSet()
-        return data
-
-    def form_valid(self, form):
-        context = self.get_context_data()
-        service_items = context['service_items']
-        with transaction.atomic():
-            self.object = form.save()
-
-            if service_items.is_valid():
-                service_items.instance = self.object
-                service_items.save()
-        return super(ReportCreateView, self).form_valid(form)
-
-    def get(self, request):
-        form = ReportCreateForm()
-        return render(request, self.template_name, context={'form': form})
-
-    def post(self, request):
-        bound_form = ReportCreateForm(request.POST)
-        if bound_form.is_valid():
-            new_report = bound_form.save(commit=False)
-            new_report.doctor = request.user.profile
-            new_report.save()
-            return redirect(new_report)
-        return render(request, self.template, context={'form': bound_form})
-'''
 
 class ReportCreateView(CreateView):
     template_name = 'reports/report_create.html'
     form_class = ReportCreateForm
+
+    def get(self, request, *args, **kwargs):
+        self.object = None
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        return self.render_to_response(self.get_context_data(form=form))
+
+    def post(self, request, *args, **kwargs):
+        self.object = None
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        service_items = self.get_context_data()['service_items']
+        if form.is_valid() and service_items.is_valid():
+            return self.form_valid(form, service_items)
+        else:
+            return self.form_invalid(form)
+
+
 
     def get_initial(self, *args, **kwargs):
         initial = super(ReportCreateView, self).get_initial(**kwargs)
@@ -72,13 +54,11 @@ class ReportCreateView(CreateView):
             data['service_items'] = ServiceItemsFormSet()
         return data
 
-    def form_valid(self, form):
+    def form_valid(self, form, service_items):
         context = self.get_context_data()
-        service_items = context['service_items']
         with transaction.atomic():
             form.instance.doctor = self.get_form_kwargs()['doctor']
             self.object = form.save()
-
             if service_items.is_valid():
                 service_items.instance = self.object
                 service_items.save()
