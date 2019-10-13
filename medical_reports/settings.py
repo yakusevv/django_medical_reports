a99951d5b63391f63c9382d7f79e16fdc9e0427b
yakusevv <yakusevv@gmail.com>
2019-10-13 17:44:58 +0200

    staticfiles rearrange

--- medical_reports/settings.py
+++ medical_reports/settings.py
@@ -172,8 +172,17 @@ THOUSAND_SEPARATOR = '.'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'assets'),
         ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_FINDERS = (
+
+'django.contrib.staticfiles.finders.FileSystemFinder',
+
+'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+
+)
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- medical_reports/urls.py
+++ medical_reports/urls.py
@@ -17,5 +17,10 @@ urlpatterns = [
     path('profiles/', include('profiles.urls'))
 ]
 
-urlpatterns += staticfiles_urlpatterns()
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    if settings.MEDIA_ROOT:
+        urlpatterns += static(
+                            settings.MEDIA_URL,
+                            document_root=settings.MEDIA_ROOT
+                            )
+    urlpatterns += staticfiles_urlpatterns()
