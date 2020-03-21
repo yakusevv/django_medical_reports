69f38bed9c0c99f20170b83e59d43b39631c0dd9
yakusevv <yakusevv@gmail.com>
2020-03-21 23:46:47 +0100

    Report requests navbar link

--- reports/urls.py
+++ reports/urls.py
@@ -18,7 +18,7 @@ from .views import (
 
 
 router = routers.DefaultRouter()
-router.register(r'requests', ReportRequestViewSet)
+router.register(r'', ReportRequestViewSet, basename='requests')
 
 urlpatterns = [
         path('', ReportsListView.as_view(), name='reports_list_url'),
@@ -33,6 +33,6 @@ urlpatterns = [
         path('<int:pk>/delete/', ReportDeleteView.as_view(), name='report_delete_url'),
         re_path('(?P<pk>\d+)/view/download/(?P<type>[a,d])/', downloadReportDocx, name='download_report_docx_url'),
         path('download_xlsx/', downloadReportsExcel, name='download_reports_xlsx_url'),
-        path('report_requests/', include(router.urls)),
+        path('report_requests/', include((router.urls, 'requests'))),
         path('report_requests/api-auth/', include('rest_framework.urls', namespace='rest_framework'))
          ]
