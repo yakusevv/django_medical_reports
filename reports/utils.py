4d6f45f85f4b9326e9414dfbfeab8a67d9b7bf1c
yakusevv <yakusevv@gmail.com>
2020-04-19 19:33:19 +0200

    Excel table formatting

--- reports/utils.py
+++ reports/utils.py
@@ -1,7 +1,7 @@
 from docxtpl import DocxTemplate, InlineImage
 from docx.shared import Mm
 from openpyxl import Workbook
-from openpyxl.styles import Font, Border, Side, PatternFill
+from openpyxl.styles import Font, Border, Side, PatternFill, Alignment
 from openpyxl.utils import get_column_letter
 
 from django.utils.translation import ugettext as _
@@ -43,21 +43,17 @@ def reports_xlsx_generator(reports):
 
     columns = [
         _('ref. number'),
-        _('Company'),
         _('Company ref. number'),
-        _('Last name'),
-        _('First name'),
-        _('Date of birth'),
+        _('Full name, date of birth'),
         _('City'),
-        _('Region'),
-        _('Doctor'),
-        _('Total price for the doctor'),
-        _('Total price')
+        _('Total price\nfor the doctor'),
+        _('Total\nprice')
     ]
 
     row_num = 2
+    col_first = 2
 
-    for col_num, column_title in enumerate(columns, 2):
+    for col_num, column_title in enumerate(columns, col_first):
         cell = worksheet.cell(row=row_num, column=col_num)
         cell.value = column_title
         cell.font = Font(bold=True)
@@ -72,54 +68,76 @@ def reports_xlsx_generator(reports):
                         end_color='f7f7f9',
                         fill_type='solid',
                     )
+        cell.alignment = Alignment(wrapText=True)
         col_letter = get_column_letter(col_num)
         if col_letter == 'B':
             column_dimensions = worksheet.column_dimensions[col_letter]
             column_dimensions.width = 20
         elif col_letter == 'C':
             column_dimensions = worksheet.column_dimensions[col_letter]
-            column_dimensions.width = 15
+            column_dimensions.width = 25
         elif col_letter == 'D':
             column_dimensions = worksheet.column_dimensions[col_letter]
-            column_dimensions.width = 15
+            column_dimensions.width = 35
         elif col_letter == 'E':
             column_dimensions = worksheet.column_dimensions[col_letter]
-            column_dimensions.width = 20
+            column_dimensions.width = 15
         elif col_letter == 'F':
             column_dimensions = worksheet.column_dimensions[col_letter]
-            column_dimensions.width = 15
+            column_dimensions.width = 10
         elif col_letter == 'G':
-            column_dimensions = worksheet.column_dimensions[col_letter]
-            column_dimensions.width = 15
-        elif col_letter == 'H':
-            column_dimensions = worksheet.column_dimensions[col_letter]
-            column_dimensions.width = 20
-        elif col_letter == 'I':
-            column_dimensions = worksheet.column_dimensions[col_letter]
-            column_dimensions.width = 20
-        elif col_letter == 'J':
             column_dimensions = worksheet.column_dimensions[col_letter]
             column_dimensions.width = 10
 
-    for report in reports:
+    for index, report in enumerate(reports):
         row_num += 1
 
         row = [
             report.get_full_ref_number,
-            report.company.name,
             report.company_ref_number,
-            report.patients_last_name,
-            report.patients_first_name,
-            report.patients_date_of_birth.strftime("%d.%m.%Y"),
+            ' '.join((
+                    str(report.patients_last_name),
+                    str(report.patients_first_name)
+                    )) + ', ' + str(report.patients_date_of_birth.strftime("%d.%m.%Y")),
             report.city.name,
-            report.city.district.region.name,
-            report.report_request.doctor.initials,
             report.get_total_price_doctor,
             report.get_total_price
         ]
 
-        for col_num, cell_value in enumerate(row, 2):
+        for col_num, cell_value in enumerate(row, col_first):
             cell = worksheet.cell(row=row_num, column=col_num)
+            cell.number_format = '0.00'
             cell.value = cell_value
+            if index == len(reports) - 1:
+                if col_num == col_first:
+                    cell.border = Border(
+                        left=Side(border_style='medium', color='FF000000'),
+                        bottom=Side(border_style='medium', color='FF000000')
+                    )
+                elif col_num == (len(row) - 1) + col_first:
+                    cell.border = Border(
+                        right=Side(border_style='medium', color='FF000000'),
+                        bottom=Side(border_style='medium', color='FF000000')
+                    )
+                else:
+                    cell.border = Border(
+                        left=Side(border_style='thin', color='FF000000'),
+                        right=Side(border_style='thin', color='FF000000'),
+                        bottom=Side(border_style='medium', color='FF000000')
+                    )
+            else:
+                if col_num == col_first:
+                    cell.border = Border(
+                        left=Side(border_style='medium', color='FF000000'),
+                    )
+                elif col_num == (len(row) - 1) + col_first:
+                    cell.border = Border(
+                        right=Side(border_style='medium', color='FF000000'),
+                    )
+                else:
+                    cell.border = Border(
+                        left=Side(border_style='thin', color='FF000000'),
+                        right=Side(border_style='thin', color='FF000000')
+                    )
 
     return workbook
